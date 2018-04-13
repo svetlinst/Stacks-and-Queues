689f2fb73e2f432fbd0507e5bde1ad49ae162a7c
Svetlin Stoyanov <svetlinst@gmail.com>
2018-04-14 00:20:24 +0300

    Final

--- P11-Poisonous-Plants/PoisonPlants.cs
+++ P11-Poisonous-Plants/PoisonPlants.cs
@@ -10,7 +10,31 @@ namespace P11_Poisonous_Plants
     {
         static void Main(string[] args)
         {
+            var numberOfPlants = int.Parse(Console.ReadLine());
+            var endNumber = 0;
+            Queue<int> plants = new Queue<int>(Console.ReadLine().Split(' ').Select(int.Parse).ToArray());
+            var startNumber = plants.Count;
+            while (startNumber!=endNumber)
+            {
+                startNumber = plants.Count;
+                for (int i = 0; i < numberOfPlants; i++)
+                {
+                    var leftPlant = plants.Dequeue();
+                    var rightPlant = plants.Dequeue();
+                    if (leftPlant > rightPlant)
+                    {
+                        plants.Enqueue(leftPlant);
+                        plants.Enqueue(rightPlant);
+                    }
+                    else
+                    {
+                        plants.Enqueue(leftPlant);
+                    }
+                }
+                endNumber = plants.Count;
+            }
 
+            Console.WriteLine(endNumber);
         }
     }
 }
