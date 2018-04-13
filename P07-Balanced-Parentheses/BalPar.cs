2a392aa087891c6b190155ffa4a3dbaae05d0b05
Svetlin Stoyanov <svetlinst@gmail.com>
2018-04-13 21:52:10 +0300

    1

---
+++ P07-Balanced-Parentheses/BalPar.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P07_Balanced_Parentheses
+{
+    class BalPar
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+            Stack<char> parenthesisStack = new Stack<char>();
+            var isBalanced = true;
+            for (int i = 0; i < input.Length; i++)
+            {
+                var currentChar = input[i];
+                if (currentChar == '(' || currentChar =='{' || currentChar =='[')
+                {
+                    parenthesisStack.Push(currentChar);
+                }
+                else if (input[0]!=')' && input[0] != ']' && input[0] != '}' && parenthesisStack.Count>0)
+                {
+                    switch (currentChar)
+                    {
+                        case ')':
+                            if (parenthesisStack.Pop() != '(')
+                            {
+                                isBalanced = false;
+                            }
+                            break;
+                        case ']':
+                            if (parenthesisStack.Pop() != '[')
+                            {
+                                isBalanced = false;
+                            }
+                            break;
+                        case '}':
+                            if (parenthesisStack.Pop() != '{')
+                            {
+                                isBalanced = false;
+                            }
+                            break;
+                    }
+                }
+                else
+                {
+                    isBalanced = false;
+                    break;
+                }
+            }
+
+            if (isBalanced)
+            {
+                Console.WriteLine("YES");
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
+        }
+    }
+}
